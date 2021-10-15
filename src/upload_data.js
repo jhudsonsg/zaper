520a70edc0e1f4c1c6ab7a0e32d9fcd44668118e
Hudson Gomes <hudson.gomes@fortics.com.br>
2021-10-15 17:18:11 -0300

    Feature: select agent for send message.

--- src/upload_data.js
+++ src/upload_data.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+const fs = require('fs');
 
 const NewBrowser = require('./NewBrowser');
 const { processoUpload } = require('./util/logger');
@@ -29,43 +29,53 @@ const deleteFile = (filePath, nameFile) => {
 
     for (agent of GROUPS_PEOPLES) {
         const [nameAgente, nameFile] = agent;
+        await page.keyboard.press('Tab');
 
+        /* EXECUTA A BUSCA */
+        await page.evaluate(async (nameAgente) => {
+            let messageBox = document.querySelectorAll("[contenteditable='true']")[0];
+            messageBox.innerHTML = nameAgente;
+            let e = document.createEvent("UIEvents");
+            e.initUIEvent("input", true, true, window, 1);
+            messageBox.dispatchEvent(e);
+        }, nameAgente);
+
+        await page.waitFor(2000);
+
+        /* CLICA NO AGENTE PESQUISADO */
         await page.evaluate(async (nameAgente) => {
             let mouseEvent = document.createEvent('MouseEvents');
             mouseEvent.initEvent('mousedown', true, true);
             document.querySelector(`[title="${nameAgente}"]`).dispatchEvent(mouseEvent);
         }, nameAgente);
 
+        /* ENVIA O TEXTO */
         await page.evaluate(async (nameAgente, nameFile) => {
-            let messageBox = document.querySelectorAll("[contenteditable='true']")[1];
-
-            while (messageBox.innerHTML == undefined) {
-                processoUpload.log('warning', 'Procurando input de envio...');
-                messageBox = document.querySelectorAll("[contenteditable='true']")[1];
-            }
-
-            messageBox.innerHTML = `Olá ${nameAgente}, segue o arquivo ${nameFile}.`;
-
             let e = document.createEvent("UIEvents");
             e.initUIEvent("input", true, true, window, 1);
+
+            let messageBox = document.querySelectorAll("[contenteditable='true']")[1];
+            messageBox.innerHTML = `Olá ${nameAgente}, segue o arquivo ${nameFile}.`;
             messageBox.dispatchEvent(e);
         }, nameAgente, nameFile);
 
+        /* ENVIA A MENSAGEM */
         await page.evaluate(async () => {
             let e = document.createEvent("MouseEvents");
             e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
             document.querySelector('span[data-icon="send"]').dispatchEvent(e);
             document.querySelector('[data-testid="clip"]').click()
         });
-
+        
+        /* ENVIA O ARQUIVO */
         const elementHandle = await page.$('[data-testid="attach-document"] + input');
         await elementHandle.uploadFile(`${PATH_DOWNLOAD_FILES}${nameFile}`);
         await page.waitForSelector('[data-testid="send"]', { visible: true })
         await page.$eval('[data-testid="send"]', btn => btn.click());
         await page.waitFor(2000)
-        
+
         processoUpload.log('info', `Arquivo ${nameFile} enviado ao agente ${nameAgente}.`);
-        
+
         await deleteFile(`${PATH_DOWNLOAD_FILES}${nameFile}`, nameFile);
     };
 
