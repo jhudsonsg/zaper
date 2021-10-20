2ec4f9efe67a8e6f401391faa473b6b9d531d482
Hudson Gomes <hudson.gomes@fortics.com.br>
2021-10-20 19:43:41 -0300

    Add cath errors and await load elements in windown

--- src/upload_data.js
+++ src/upload_data.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const NewBrowser = require('./NewBrowser');
 const { processoUpload } = require('./util/logger');
 const { GROUPS_PEOPLES, PATH_DOWNLOAD_FILES } = require('../config');
+const { throws } = require('assert');
 
 
 const deleteFile = (filePath, nameFile) => {
@@ -15,71 +16,105 @@ const deleteFile = (filePath, nameFile) => {
 
 (async () => {
     const browser = await NewBrowser();
+
     const page = await browser.newPage();
     await page.setViewport({ width: 1200, height: 800 });
+    await page.goto('https://web.whatsapp.com/', { waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'] });
 
-    await page.goto('https://web.whatsapp.com/', {
-        waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2']
-    });
-
-    await page.waitFor(2000)
     await page.waitForSelector('[contenteditable="true"]', { visible: true })
+    await page.waitFor(5000);
 
     processoUpload.log('info', 'Iniciando processo.');
 
     for (agent of GROUPS_PEOPLES) {
+        await page.waitFor(5000);
         const [nameAgente, nameFile] = agent;
         await page.keyboard.press('Tab');
 
         /* EXECUTA A BUSCA */
-        await page.evaluate(async (nameAgente) => {
-            let messageBox = document.querySelectorAll("[contenteditable='true']")[0];
-            messageBox.innerHTML = nameAgente;
-            let e = document.createEvent("UIEvents");
-            e.initUIEvent("input", true, true, window, 1);
-            messageBox.dispatchEvent(e);
-        }, nameAgente);
+        try {
+            await page.evaluate(async (nameAgente) => {
+                let e = document.createEvent("UIEvents");
+                e.initUIEvent("input", true, true, window, 1);
+
+                let searchAgent = document.querySelectorAll("[contenteditable='true']")[0];
+                searchAgent.innerHTML = nameAgente;
+                searchAgent.dispatchEvent(e);
+            }, nameAgente);
+        } catch (error) {
+            console.log(`Não foi possivel realizar a busca ao agente ${nameAgente}.`);
+            processoUpload.log('error', `Não foi possivel realizar a busca ao agente ${nameAgente}.`);
+            processoUpload.log('error', error.message);
+        }
 
         await page.waitFor(2000);
 
         /* CLICA NO AGENTE PESQUISADO */
-        await page.evaluate(async (nameAgente) => {
-            let mouseEvent = document.createEvent('MouseEvents');
-            mouseEvent.initEvent('mousedown', true, true);
-            document.querySelector(`[title="${nameAgente}"]`).dispatchEvent(mouseEvent);
-        }, nameAgente);
+        try {
+            await page.evaluate(async (nameAgente) => {
+                let mouseEvent = document.createEvent('MouseEvents');
+                mouseEvent.initEvent('mousedown', true, true);
+                document.querySelector(`[title="${nameAgente}"]`).dispatchEvent(mouseEvent);
 
-        /* ENVIA O TEXTO */
-        await page.evaluate(async (nameAgente, nameFile) => {
-            let e = document.createEvent("UIEvents");
-            e.initUIEvent("input", true, true, window, 1);
+            }, nameAgente);
+        } catch (error) {
+            console.log(`Não foi possivel clicar no agente ${nameAgente}.`);
+            processoUpload.log('error', `Não foi possivel clicar no agente ${nameAgente}.`);
+            processoUpload.log('error', error.message);
+        }
 
-            let messageBox = document.querySelectorAll("[contenteditable='true']")[1];
-            messageBox.innerHTML = `Olá ${nameAgente}, segue o arquivo ${nameFile}.`;
-            messageBox.dispatchEvent(e);
-        }, nameAgente, nameFile);
+        await page.waitForSelector('[data-testid="clip"]', { visible: true })
 
-        /* ENVIA A MENSAGEM */
-        await page.evaluate(async () => {
-            let e = document.createEvent("MouseEvents");
-            e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            document.querySelector('span[data-icon="send"]').dispatchEvent(e);
-            document.querySelector('[data-testid="clip"]').click()
-        });
-        
+        try {
+            /* ENVIA O TEXTO */
+            await page.evaluate(async (nameAgente, nameFile) => {
+                let e = document.createEvent("UIEvents");
+                e.initUIEvent("input", true, true, window, 1);
+
+                let messageBox = document.querySelectorAll("[contenteditable='true']")[1];
+                messageBox.innerHTML = `Olá ${nameAgente}, segue o arquivo ${nameFile}.`;
+                messageBox.dispatchEvent(e);
+
+            }, nameAgente, nameFile);
+        } catch (error) {
+            console.log(`Não foi possivel selecionar a caixa de texto do agente ${nameAgente}.`);
+            processoUpload.log('error', `Não foi possivel selecionar a caixa de texto do agente ${nameAgente}.`);
+            processoUpload.log('error', error.message);
+        }
+
+        try {
+            /* ENVIA A MENSAGEM */
+            await page.evaluate(async () => {
+                let e = document.createEvent("MouseEvents");
+                e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                document.querySelector('span[data-icon="send"]').dispatchEvent(e);
+                document.querySelector('[data-testid="clip"]').click()
+            });
+        } catch (error) {
+            console.log(`Não foi possivel clicar para enviar sua mensagem ao agente ${nameAgente}.`);
+            processoUpload.log('error', `Não foi possivel clicar para enviar sua mensagem ao agente ${nameAgente}.`);
+            processoUpload.log('error', error.message);
+        }
+
         /* ENVIA O ARQUIVO */
-        const elementHandle = await page.$('[data-testid="attach-document"] + input');
-        await elementHandle.uploadFile(`${PATH_DOWNLOAD_FILES}${nameFile}`);
-        await page.waitForSelector('[data-testid="send"]', { visible: true })
-        await page.$eval('[data-testid="send"]', btn => btn.click());
-        await page.waitFor(2000)
+        try {
+            const elementHandle = await page.$('[data-testid="attach-document"] + input');
+            await elementHandle.uploadFile(`${PATH_DOWNLOAD_FILES}${nameFile}`);
+            await page.waitForSelector('[data-testid="send"]', { visible: true })
+            await page.$eval('[data-testid="send"]', btn => btn.click());
+            await page.waitFor(2000)
+        } catch (error) {
+            console.log(`Não foi possivel envia o arquivo ao agente ${nameAgente}.`);
+            processoUpload.log('error', `Não foi possivel envia o arquivo ao agente ${nameAgente}.`);
+            processoUpload.log('error', error.message);
+        }
 
-        processoUpload.log('info', `Arquivo ${nameFile} enviado ao agente ${nameAgente}.`);
+        processoUpload.log('info', `Processo da tentativa de envio do arquivo ${nameFile} ao agente ${nameAgente} finalizado.`);
 
+        await page.keyboard.press('Tab');
         await deleteFile(`${PATH_DOWNLOAD_FILES}${nameFile}`, nameFile);
     };
 
-    await page.waitFor(2000)
     await browser.close();
 
     processoUpload.log('info', 'Processo finalizado.');
